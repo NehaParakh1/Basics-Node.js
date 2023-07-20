aa62a37271e1ae7086ee0ca649527ccb08b1c1aa
NehaParakh1 <nehadugar3@gmail.com>
2023-07-20 12:36:48 +0530

    routing task 7

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url=req.url;
+    if(url==="/home"){
+        res.write('<html>')
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><h1>Welcome Home</h1></body>');
+        res.write('</html>');
+        return res.end();  
+    }
+    if(url==="/About"){
+        res.write('<html>')
+        res.write('<head><title>About Page</title></head>');
+        res.write('<body><h1>Welcome to AboutUs Page</h1></body>');
+        res.write('</html>');
+        return res.end();  
+    }
+    if(url==="/node"){
+        res.write('<html>')
+        res.write('<head><title>Node Page</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();  
+    }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>')
   res.write('<head><title>My First Page</title></head>');
