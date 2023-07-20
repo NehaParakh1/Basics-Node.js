a3581744abd2a4ec9b1465583ec1ff19403933c0
NehaParakh1 <nehadugar3@gmail.com>
2023-07-20 14:05:44 +0530

    creating file with message

--- server.js
+++ server.js
@@ -1,35 +1,56 @@
 const http = require('http');
 
+const fs=require('fs');
+
 const server = http.createServer((req, res) => {
     const url=req.url;
-    if(url==="/home"){
-        res.write('<html>')
-        res.write('<head><title>Home Page</title></head>');
-        res.write('<body><h1>Welcome Home</h1></body>');
-        res.write('</html>');
-        return res.end();  
+    
+    const method=req.method;
+
+    if(url==='/'){
+        fs.readFile('message.txt', {encoding: 'utf-8'},
+        (err,data)=>{
+           if(err){
+            console.log(err)
+           }
+
+           res.setHeader('Content-Type', 'text/html');
+           res.write('<html>');
+           res.write('<head> <title> Message Input </title></head>');
+           res.write(`<body>${data}</body>`)
+           res.write('<body><form action="/message" method="POST" ><input type="text" name="message"><button type="submit">Send</button></form></body');
+           res.write('<html>')
+           return res.end();
+        })
     }
-    if(url==="/About"){
-        res.write('<html>')
-        res.write('<head><title>About Page</title></head>');
-        res.write('<body><h1>Welcome to AboutUs Page</h1></body>');
-        res.write('</html>');
-        return res.end();  
+
+    else if(url==='/message' && method==='POST'){
+        const body=[];
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+            console.log(body);
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            const message=parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode=302;
+        res.setHeader('Location', '/');
+        return res.end();
     }
-    if(url==="/node"){
+
+    else{
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>')
-        res.write('<head><title>Node Page</title></head>');
-        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+        res.write('<head><title>Home Page</title> </head>');
+        res.write('<body><h1>Welcome to Home Page</h1></body>');
         res.write('</html>');
-        return res.end();  
+        res.end();
     }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>')
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server! </h1></body>');
-  res.write('</html>');
-  res.end();
-});
+})
+    
 
 server.listen(4000, () => {
   console.log('Server running at http://localhost:4000/');
