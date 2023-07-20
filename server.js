e1fee36e81f961f190362313d98fe58e2deeffb5
NehaParakh1 <nehadugar3@gmail.com>
2023-07-20 12:17:21 +0530

    html in node.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('My name is Neha Parakh');
-  console.log('My name is Neha Parakh');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>')
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server! </h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(4000, () => {
